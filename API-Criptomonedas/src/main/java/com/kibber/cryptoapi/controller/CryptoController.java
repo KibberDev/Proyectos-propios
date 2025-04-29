1bf38433fb763781bbd6a1a87c471aa5e241112e
KibberDev <osgomez.dev@gmail.com>
2025-04-29 12:26:09 +0200

    Actualizados archivos de API y añadido Tres en Raya

--- API-Criptomonedas/src/main/java/com/kibber/cryptoapi/controller/CryptoController.java
+++ API-Criptomonedas/src/main/java/com/kibber/cryptoapi/controller/CryptoController.java
@@ -5,14 +5,13 @@ import com.kibber.cryptoapi.model.Criptomoneda;
 import com.kibber.cryptoapi.service.CryptoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+//prueba
 @RestController
 @RequestMapping("/api/criptomonedas")
 public class CryptoController {
--- API-Criptomonedas/src/main/java/com/kibber/cryptoapi/model/Criptomoneda.java
+++ API-Criptomonedas/src/main/java/com/kibber/cryptoapi/model/Criptomoneda.java
@@ -6,8 +6,7 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Setter
-@Getter
+//Getters y Setter a menos
 @Entity
 @Table(name="criptomonedas")
 
@@ -17,16 +16,22 @@ public class Criptomoneda {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(name = "nombre")
     private String nombre;
 
+    @Column(name = "simbolo")
     private String simbolo;
 
+    @Column(name = "precio_actual")
     private double precioActual;
 
+    @Column (name = "cambio_24h")
     private double cambio24h;
 
+    @Column (name = "volumen_mercado")
     private Long volumenMercado;
 
+    @Column(name = "fecha_ultima_actualizacion")
     private LocalDate fechaUltimaActualizacion;
 
     public Criptomoneda() {
@@ -39,6 +44,62 @@ public class Criptomoneda {
         this.precioActual = precioActual;
         this.cambio24h = cambio24h;
         this.volumenMercado = volumenMercado;
+        this.fechaUltimaActualizacion = fechaUltimaActualizacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSimbolo() {
+        return simbolo;
+    }
+
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    public double getPrecioActual() {
+        return precioActual;
+    }
+
+    public void setPrecioActual(double precioActual) {
+        this.precioActual = precioActual;
+    }
+
+    public double getCambio24h() {
+        return cambio24h;
+    }
+
+    public void setCambio24h(double cambio24h) {
+        this.cambio24h = cambio24h;
+    }
+
+    public Long getVolumenMercado() {
+        return volumenMercado;
+    }
+
+    public void setVolumenMercado(Long volumenMercado) {
+        this.volumenMercado = volumenMercado;
+    }
+
+    public LocalDate getFechaUltimaActualizacion() {
+        return fechaUltimaActualizacion;
+    }
+
+    public void setFechaUltimaActualizacion(LocalDate fechaUltimaActualizacion) {
         this.fechaUltimaActualizacion = fechaUltimaActualizacion;
     }
 }
